9d5e83e9de6aacd2632742668fbe15b9b4636c88
RitishSahu <118614372+RitishSahu@users.noreply.github.com>
2022-12-03 20:01:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var x=document.getElementById("myAudio1");
+var y=document.getElementById("myAudio2");
+var z=document.getElementById("myAudio3");
+var a=document.getElementById("myAudio4");
+var b=document.getElementById("myAudio5");
+var c=document.getElementById("myAudio6");
+var d=document.getElementById("myAudio7");
+
+function start(){
+    x.play();
+}
+function stopit(){
+    x.pause();
+}
+
+function startsong(){
+    y.play();
+}
+function stopsong(){
+    y.pause();
+}
+
+function startmusic(){
+    z.play();
+}
+function stopmusic(){
+    z.pause();
+}
+
+function starton(){
+    a.play();
+}
+function stopon(){
+    a.pause();
+}
+
+function startbts(){
+    b.play();
+}
+function stopbts(){
+    b.pause();
+}
+
+function startdev(){
+    c.play();
+}
+function stopdev(){
+    c.pause();
+}
+
+function start(){
+    d.play();
+}
+function stopit(){
+    d.pause();
+}
